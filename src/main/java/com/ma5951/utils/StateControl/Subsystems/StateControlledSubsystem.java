5ef3a00d56ecf97301289f1b12cb4de8a7ab3843
GalDotan <gldotan@gmail.com>
2024-08-19 16:47:26 +0300

    Added MALib

---
+++ src/main/java/com/ma5951/utils/StateControl/Subsystems/StateControlledSubsystem.java
@@ -0,0 +1,87 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.ma5951.utils.StateControl.Subsystems;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+import com.ma5951.utils.StateControl.StatesTypes.State;
+import com.ma5951.utils.StateControl.StatesTypes.StatesConstants;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+/** Add your docs here. */
+public abstract class StateControlledSubsystem extends SubsystemBase {
+
+    private State[] systemStates;
+    private List<State> systemStatesList;
+    private State systemEnableState = StatesConstants.SYSTEM_ENABLE;
+    private State systemFunctionState = StatesConstants.AUTOMATIC;
+    private State targetState;
+    private State travelState;
+    private Supplier<Boolean> measurmentSupplier;
+    private SubsystemStateMeachin subsystemStateMeachin;
+
+    public void setSystemStates(State[] states ,State travelState ,Supplier<Boolean> supplier) {
+        systemStates = states;
+        systemStatesList = Arrays.asList(systemStates);
+        this.travelState = travelState;
+        measurmentSupplier = supplier;
+    }
+
+    public void setStateMeachin(SubsystemStateMeachin meachin) {
+        subsystemStateMeachin = meachin;
+    }
+
+    public SubsystemStateMeachin getStateMeachin() {
+        return subsystemStateMeachin;
+    }
+
+    public void disableSystem() {
+        systemEnableState = StatesConstants.SYSTEM_DISABLED;
+    }
+
+    public void enableSystem() {
+        systemEnableState = StatesConstants.SYSTEM_ENABLE;
+    }
+
+    public State getSystemEnablState() {
+        return systemEnableState;
+    }
+
+    public void setSystemFunctionState(State FunctioState) {
+        systemFunctionState = FunctioState;
+    }
+
+    public State getSystemFunctionState() {
+        return systemFunctionState;
+    }
+
+    public State[] getSystemStates() {
+        return systemStates;
+    }
+
+    public State getTargetState() {
+        return targetState;
+    }
+
+    public void setTargetState(State state) {
+        if (systemStatesList.contains(state)) {
+            targetState = state;
+        } else {
+            System.err.println("Can set target state for " + getSubsystem());
+        }
+    }
+
+    public State getCurrenState() {
+        if (measurmentSupplier.get()) {
+            return targetState;
+        } else {
+            return travelState;
+        }
+    }
+
+}
---
+++ src/main/java/com/ma5951/utils/StateControl/Subsystems/SubsystemStateMeachin.java
@@ -0,0 +1,63 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package com.ma5951.utils.StateControl.Subsystems;
+
+import com.ma5951.utils.StateControl.RobotState.RobotStates;
+
+public abstract class SubsystemStateMeachin {
+
+    public StateControlledSubsystem subsystem;
+    public void setSubsystem(StateControlledSubsystem system) {
+        subsystem = system;
+    }
+
+    public void SystemLoop() {
+        switch (RobotStates.getRobotEnableState().getName()) {
+            case "ROBOT_ENABLE":
+                switch (subsystem.getSystemEnablState().getName()) {
+                    case "SYSTEM_ENABLE":
+                        switch (RobotStates.getRobotFunctionState().getName()) {
+                            case "TELEOP":
+                                TeleopLoop();
+                                break;
+                            case "AUTO":
+                                AutoLoop();
+                                break;
+                            case "TEST":
+                                TestLoop();
+                                break;
+                        }
+                        break;
+                
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void TeleopLoop() {
+        switch (subsystem.getSystemFunctionState().getName()) {
+            case "AUTOMATIC":
+                AutomaticLoop();
+                break;
+        
+            case "MANUEL":
+                ManuelLoop();
+                break;
+        }
+    }
+
+    public abstract void AutoLoop();
+
+    public abstract void TestLoop() ;
+
+    public abstract void AutomaticLoop(); 
+
+    public abstract void ManuelLoop() ;
+
+}
