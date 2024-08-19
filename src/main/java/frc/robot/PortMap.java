346e227ffed7447fb8a0fe38acb8f038f8772693
GalDotan <gldotan@gmail.com>
2024-08-19 18:16:11 +0300

    Work on IntakeIOs

---
+++ src/main/java/frc/robot/PortMap.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+public class PortMap {
+
+    public class Swerve {
+
+    }
+
+    public class Intake {
+        public static final int KrakenIntakeMotor  = 14;
+    }
+
+    public class Feeder {
+        public static final int KrakenFeederMotor = 18;
+        public static final int DIO_FeederSensor = 1;
+    }
+
+    public class Shooter {
+        
+    }
+
+    public class Arm {
+        
+    }
+
+    public class Vision {
+
+    }
+
+    public class Leds {
+
+    }
+}
